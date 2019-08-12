edde81d9d66f69489582eac5fa4c1da14699f299
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-12 20:00:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,5 +18,5 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find {|n| n.include?(cheese_types)}
+  array.find {|n| cheese_types.include?(n)}
 end
